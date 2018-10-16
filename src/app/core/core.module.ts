fda59c2538fc54ff0fcf65594ac73ea7c3341bcc
asirko <sirko.alexandre@gmail.com>
2018-10-16 14:02:19 +0200

    test

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,11 +5,13 @@ import { CoreRoutingModule } from './core-routing.module';
 import { LayoutComponent } from './layout/layout.component';
 import { FadeOnListComponent } from './layout/fade-on-list/fade-on-list.component';
 import { DelayAsyncComponent } from './layout/delay-async/delay-async.component';
+import { MarkdownModule } from 'ngx-markdown';
 
 @NgModule({
   imports: [
     CommonModule,
     CoreRoutingModule,
+    MarkdownModule.forRoot(),
   ],
   exports: [
     CoreRoutingModule
